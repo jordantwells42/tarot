00162a936a600e7f413558616348a8ddc8c460a7
Jordan Wells <jordantwells@gmail.com>
2022-07-04 14:29:04 -0400

    update text styles

--- src/components/board.js
+++ src/components/board.js
@@ -51,7 +51,7 @@ export default function Board () {
         ref={boardRef}
         className='w-full h-screen bg-slate-500 flex flex-col items-center justify-center'
       >
-        <h1 className="text-6xl pt-20 text-center text-slate-900 font-bold">Tarot Reading</h1>
+        <h1 className="text-6xl pt-20 text-center text-slate-200 font-bold">Tarot Reading</h1>
         <div className='w-full flex h-[80%] flex-row items-center justify-center'>
         {cards &&
           cards.map((card, idx) => {
@@ -71,13 +71,13 @@ export default function Board () {
         <div className='pb-10 flex flex-row gap-10'>
           <button
             onClick={refresh}
-            className='border-2 rounded-2xl border-slate-900 p-4 text-white font-bold'
+            className='border-2 rounded-2xl border-slate-700 p-4 text-slate-200 font-bold'
           >
             New Cards
           </button>
           <button
             onClick={toPositions}
-            className='border-2 rounded-2xl border-slate-900 p-4 text-white font-bold'
+            className='border-2 rounded-2xl border-slate-700 p-4 text-slate-200 font-bold'
           >
             Get Reading
           </button>
